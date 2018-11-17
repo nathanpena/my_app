dc650de02c0f7cc4978b560086e52d0b5a378152
nnpena <nnpena@outlook.com>
2018-11-17 01:45:19 +0000

    updated gemfile for deployment to Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    
+    def hello
+        render text: "<h2>hello</h2><p>Welcome HOme</p>"
+    end
 end

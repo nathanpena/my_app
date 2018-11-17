78a466119fd8d4e7c9e12ec6095b56b9193bc07a
nnpena <nnpena@outlook.com>
2018-11-17 15:05:03 +0000

    save cre

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     
     def hello
-        render plain: "<h2>hello</h2><p>Welcome HOme</p>"
+        render plain: "<h2>hello</h2><p>Welcome Home</p>"
     end
 end
